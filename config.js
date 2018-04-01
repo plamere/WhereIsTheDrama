817b5dbad14dfb594975c49a39e5578a4e797470
Paul Lamere <paull@spotify.com>
2018-04-01 07:55:27 -0400

    improved sharing

---
+++ config.js
@@ -0,0 +1,5 @@
+"use strict";
+var SPOTIFY_CLIENT_ID = 'a85dcd39957247aca71b059f4ac15b3c';
+var REMOTE_SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/WheresTheDrama/'
+var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
+var SPOTIFY_REDIRECT_URI = REMOTE_SPOTIFY_REDIRECT_URI;
